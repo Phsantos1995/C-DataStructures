1bee20c5dbcd8e125bdb3bce1df7c76d4fe30267
Phsantos1995 <>
2021-04-13 17:21:29 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class MyClass{
+  public :
+   int myNum;
+    string myString;
+
+};
+
+
+int main() {
+ MyClass myobj;
+
+
+ myobj.myNum=39;
+ myobj.myString="Some text";
+
+
+
+
+cout << myobj.myNum <<"\n";
+cout <<myobj.myString;
+
+return 0;
+}
